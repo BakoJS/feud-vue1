930ff44cffba9b9949f3bf64d06855cb7f666c26
Scott Steinbeck <uniquetrio2000@gmail.com>
2018-06-18 20:26:43 -0700

    Setup page to allow for switched users, implement vue-resource, add voting methods

--- js/index.js
+++ js/index.js
@@ -1,106 +1,117 @@
 var feud_vue = new Vue({
-    el: '#feud_admin',
+  el: "#feud_admin",
   data: {
-    Questions: [
-      {
-        QuestionID: 1,
-        QuestionText: "What is the Best Javascript Library",
-        Answers: [
-          {
-            AnswerID: 1,
-            AnswerText: "Vue JS",
-            AnswerVotes: 0
-          },
-          {
-            AnswerID: 2,
-            AnswerText: "React",
-            AnswerVotes: 0
-          },
-          {
-            AnswerID: 3,
-            AnswerText: "Angular JS",
-            AnswerVotes: 0
-          },
-          {
-            AnswerID: 4,
-            AnswerText: "JQuery",
-            AnswerVotes: 0
-          },
-          {
-            AnswerID: 6,
-            AnswerText: "Ext JS",
-            AnswerVotes: 0
-          },
-          {
-            AnswerID: 7,
-            AnswerText: "Backbone",
-            AnswerVotes: 1
-          }
-        ]
-      },
-      {
-        QuestionID: 2,
-        QuestionText: "How many JS libraries is the perfect amount",
-        Answers: []
-      },
-      {
-        QuestionID: 3,
-        QuestionText: "Code optimization, what makes the biggest impact",
-        Answers: []
-      },
-      {
-        QuestionID: 4,
-        QuestionText: "What is the best database engine",
-        Answers: []
-      },
-      {
-        QuestionID: 5,
-        QuestionText: "Best way to search for solutions",
-        Answers: []
-      },
-      {
-        QuestionID: 6,
-        QuestionText: "Best source control tool",
-        Answers: []
-      },
-      {
-        QuestionID: 7,
-        QuestionText: "What is the most secure encryption",
-        Answers: []
-      },
-      {
-        QuestionID: 8,
-        QuestionText: "When should you decide to create a microservice",
-        Answers: []
-      },
-      {
-        QuestionID: 9,
-        QuestionText: "What is the best testing suite",
-        Answers: []
-      }
+    activeUser: 1,
+    userList: [
+      { id: 1, name: "Admin" },
+      { id: 2, name: "User 1" },
+      { id: 3, name: "User 2" },
+      { id: 4, name: "User 3" },
+      { id: 5, name: "User 4" }
     ],
-    idxQuestions:{},
-    activeQuestion: 1,
-    upvotedAnswers: {
-        1: true,
-        3: true,
-        4: true,
-        6: true,
-        11: true
-    }
+    questionDB: [],
+    searchText: "",
+    idxQuestions: {},
+    formData: {
+      QuestionID: 0,
+      QuestionText: "",
+      AnswerText: ""
+    },
+    upvotedAnswers: []
   },
   http: {},
   created: function() {
     var _self = this;
+    this.changeUser(1);
     this.idxQuestions = {};
-    this.Questions.forEach(function (q,i) {
+    this.$http.get("http://localhost:3000/questions").then(function(res) {
+      _self.questionDB = res.body;
+      _self.questionDB.forEach(function(q, i) {
         _self.idxQuestions[q.QuestionID] = q;
-    })
-    console.log(_self.idxQuestions);
+      });
+    });
   },
   methods: {
-    'checkVote': function (answer) {
-        return this.upvotedAnswers.hasOwnProperty(answer.AnswerID) ? 'fas' : 'far';
+    changeUser: function(userID) {
+      this.activeUser = userID;
+      this.getUserVotes();
+    },
+    getUserVotes: function() {
+      var _self = this;
+      this.$http
+        .post("http://localhost:3000/uservotes", { userID: this.activeUser })
+        .then(function(res) {
+          console.log(res);
+          _self.upvotedAnswers = res.body;
+        });
+    },
+    checkVote: function(answer) {
+      return this.upvotedAnswers.indexOf(answer.AnswerID) != -1 ? "fas" : "far";
+    },
+    editQuestion: function(question) {
+      this.formData.QuestionID = question.QuestionID;
+      this.formData.QuestionText = question.QuestionText;
+      this.formData.AnswerText = "";
+    },
+    saveQuestion: function() {
+      var _self = this;
+      this.$http
+        .post("http://localhost:3000/question", {
+          questionID: this.formData.QuestionID,
+          questionText: this.formData.QuestionText
+        })
+        .then(function(res) {
+          var result = res.body[0];
+          console.log(result);
+          for (var i in result) {
+            _self.idxQuestions[result.QuestionID][i] = result[i];
+          }
+        });
+    },
+    getQuestionAnswers: function() {
+      if (this.idxQuestions.hasOwnProperty(this.formData.QuestionID))
+        return this.idxQuestions[this.formData.QuestionID].Answers;
+      return [];
+    },
+    addAnswer: function() {
+      if (this.formData.AnswerText == "") return false;
+      var _self = this;
+      this.$http
+        .post("http://localhost:3000/answer", {
+          questionID: this.formData.QuestionID,
+          answerText: this.formData.AnswerText,
+          userID: this.activeUser
+        })
+        .then(function(res) {
+          var result = res.body;
+
+          _self.idxQuestions[_self.formData.QuestionID]["Answers"].push(result);
+          _self.upvotedAnswers.push(result.AnswerID);
+          _self.formData.AnswerText = "";
+        });
+    },
+    toggleVote: function(answer) {
+      var _self = this;
+      var wasUpvoted = this.upvotedAnswers.indexOf(answer.AnswerID) != -1;
+      var endpoint = wasUpvoted ? "unvote" : "vote";
+      this.$http
+        .post("http://localhost:3000/" + endpoint, {
+          answerID: answer.AnswerID,
+          userID: this.activeUser
+        })
+        .then(function(res) {
+          var result = res.body;
+            console.log(result);
+          for (var i in result) {
+            answer[i] = result[i];
+          }
+
+          if (wasUpvoted) {
+            _self.upvotedAnswers.splice(_self.upvotedAnswers.indexOf(answer.AnswerID),1);
+          } else {  
+            _self.upvotedAnswers.push(answer.AnswerID)
+          }
+        });
     }
   }
 });
